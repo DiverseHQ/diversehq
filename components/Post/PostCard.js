59e9ccb3f4636ee5acc820fb68bc00026d5bc7f7
RathodDeven <devenrathodrd@gmail.com>
2022-05-15 17:22:54 +0530

    pushing baby

--- components/Post/PostCard.js
+++ components/Post/PostCard.js
@@ -4,16 +4,16 @@ const PostCard = ({post}) => {
     console.log("post",post);
   return (
     <div className="w-[450px] bg-secondary-bg rounded-[20px] p-3.5">
-        <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-row justify-between items-center mb-2">
             <div>{post.communityId} by {post.author.slice(0,5)}...</div>
             <div>{post.createdAt}</div>
         </div>
-        <div className="flex flex-row justify-between items-center">
-            <div>{post.likes.length} {post.title}</div>
+        <div className="flex flex-row justify-between items-center mb-2">
+            <div className="flex flex-row"><img src="/love.png" className="w-5 mr-2" /> {post.likes.length} {post.title}</div>
             <div></div>
         </div>
         <img src={post.postImageUrl} width="400px" alt="post" className="rounded-[16px]"/>
-        <div className="flex flex-row justify-between items-center m-2  ">
+        <div className="flex flex-row justify-between items-center mt-2  ">
             <div className="flex flex-row"><img src="/comment.png" className="w-5 mr-1" /> {post.comments.length}</div>
             <div className="flex flex-row">
                 <button><img src="/applause.png" className="w-5 mr-1" /></button> 
