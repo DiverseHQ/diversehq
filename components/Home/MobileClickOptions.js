451397f78a5a4704681cd01e9144a2206e88b8f7
RathodDeven <devenrathodrd@gmail.com>
2022-07-20 23:04:47 +0530

    redirect to u/waddress on clicking your profile

--- components/Home/MobileClickOptions.js
+++ components/Home/MobileClickOptions.js
@@ -1,9 +1,18 @@
+import { useRouter } from 'next/router';
 import React from 'react'
+import { useProfile } from '../../utils/WalletContext'
 
 const MobileClickOptions = () => {
+  const router = useRouter();
+  const { user } = useProfile();
+  const routeToUserProfile = () => {
+    if (user) {
+      router.push(`/u/${user.walletAddress}`)
+    }
+  }
   return (
     <div className='fixed top-14 left-2.5 z-10'>
-        <div className='px-3 py-2 bg-s-bg rounded-full my-2 button-dropshadow'>Visit Profile</div>
+        <div className='px-3 py-2 bg-s-bg rounded-full my-2 button-dropshadow' onClick={routeToUserProfile}>Visit Profile</div>
         <div className='px-3 py-2 bg-s-bg rounded-full my-2 button-dropshadow'>Edit Profile</div>
         <div className='px-3 py-2 bg-s-bg rounded-full my-2 button-dropshadow'>Create Community</div>
         <div className='px-3 py-2 bg-s-bg rounded-full my-2 button-dropshadow'>Disconnect</div>
