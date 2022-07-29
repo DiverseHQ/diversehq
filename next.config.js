15f0e6c8288d384c6b1e489d06b82ee04c0ec67f
Meta-K <kirtiraj3738@gmail.com>
2022-07-29 19:32:53 +0530

    ToplevelAwait true

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["dweb.link"],
-  }
+  },
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true }};
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true 
+    return config;
+  },
+
 }
 
 module.exports = nextConfig
