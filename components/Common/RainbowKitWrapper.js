8d105834f8d99b1675327b3f95381918008d6d13
RathodDeven <devenrathodrd@gmail.com>
2022-11-22 16:39:56 +0530

    fixed alchemy calls repeatedly

--- components/Common/RainbowKitWrapper.js
+++ components/Common/RainbowKitWrapper.js
@@ -1,14 +1,22 @@
 import React from 'react'
 
 import '@rainbow-me/rainbowkit/styles.css'
-import { getDefaultWallets, RainbowKitProvider, midnightTheme } from '@rainbow-me/rainbowkit'
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  midnightTheme
+} from '@rainbow-me/rainbowkit'
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
 
 const { chains, provider } = configureChains(
   [chain.polygonMumbai],
-  [alchemyProvider({ alchemyId: process.env.NEXT_PUBLIC_NODE_ENV }), publicProvider()]
+  [
+    // process.env.NEXT_PUBLIC_ALCHEMY_ID
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
+    publicProvider()
+  ]
 )
 
 const { connectors } = getDefaultWallets({
@@ -25,9 +33,9 @@ const wagmiClient = createClient({
 const RainbowKitWrapper = ({ children }) => {
   return (
     <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider coolMode chains={chains} theme={midnightTheme()}>
-            {children}
-        </RainbowKitProvider>
+      <RainbowKitProvider coolMode chains={chains} theme={midnightTheme()}>
+        {children}
+      </RainbowKitProvider>
     </WagmiConfig>
   )
 }
