e77ee87d4583fa658f1631d5668a0a1acd30075e
RathodDeven <devenrathodrd@gmail.com>
2023-01-02 17:24:19 +0530

    communities scrollable

--- components/Post/NavFilterAllPosts.js
+++ components/Post/NavFilterAllPosts.js
@@ -83,7 +83,7 @@ const NavFilterAllPosts = () => {
           <RiArrowDropDownLine className="w-6 h-6 text-p-btn items-center" />
         </button>
         <div
-          className="bg-s-bg rounded-md sm:rounded-xl absolute mt-7 z-50"
+          className="bg-s-bg rounded-md sm:rounded-xl absolute mt-7 z-50 max-h-[500px] overflow-y-auto overflow-x-hidden"
           ref={dropdownRef}
         >
           {showJoinedCommunities &&
