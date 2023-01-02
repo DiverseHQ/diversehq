351edb778994e17d84483d963bdd1eba6b3a2235
RathodDeven <devenrathodrd@gmail.com>
2023-01-02 12:23:24 +0530

    fixed top and nav in explore feed nav

--- components/Explore/ExploreFeedNav.js
+++ components/Explore/ExploreFeedNav.js
@@ -23,7 +23,7 @@ const ExploreFeedNav = () => {
 
   return (
     <div className="font-bold text-sm sm:text-base flex flex-row  border px-3 sm:px-6 bg-white mt-4 sm:mt-0 mb-4 py-1 sm:py-3 w-full   sm:rounded-xl space-x-4 sm:space-x-9 items-center">
-      <div
+      <button
         className={`flex p-1 sm:py-1 sm:px-2 items-center hover:cursor-pointer gap-2 rounded-md sm:rounded-xl ${
           active === 'top' && 'bg-p-bg'
         }  hover:bg-p-btn-hover`}
@@ -32,22 +32,19 @@ const ExploreFeedNav = () => {
         }}
       >
         <MdLeaderboard />
-        <button>Top</button>
-      </div>
-      <div
+        <div>Top</div>
+      </button>
+      <button
         className={`flex p-1 sm:py-1 sm:px-2 items-center hover:cursor-pointer gap-2 rounded-md sm:rounded-xl ${
           active === 'new' && 'bg-p-bg'
         }  hover:bg-p-btn-hover`}
+        onClick={() => {
+          router.push('/explore/new')
+        }}
       >
         <HiSparkles />
-        <button
-          onClick={() => {
-            router.push('/explore/new')
-          }}
-        >
-          New
-        </button>
-      </div>
+        <div>New</div>
+      </button>
 
       {/* <div
         className={`flex items-center hover:cursor-pointer gap-2 py-1 px-2 rounded-full ${
