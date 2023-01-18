21a0d6e6119fceaefe2d6270540f0f93fab77e65
RathodDeven <devenrathodrd@gmail.com>
2023-01-18 22:02:13 +0530

    notification off-chain fix

--- components/Notification/NotificationCard.js
+++ components/Notification/NotificationCard.js
@@ -8,7 +8,9 @@ import NotificationPostUpvote from './NotificationPostUpvote'
 const NotificationCard = ({ notification }) => {
   return (
     <div
-      className={`my-4 px-3 py-2 sm:p-3 sm:rounded-xl border-b sm:border-none shadow-sm w-full `}
+      className={
+        'my-4 px-3 py-2 sm:p-3 sm:bg-s-bg sm:rounded-xl border-b sm:border-none shadow-sm w-full'
+      }
     >
       {notification.type === notificationTypes.COMMENT && (
         <NotificationCommentedPostCard notification={notification} />
