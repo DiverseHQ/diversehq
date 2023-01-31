73fc96f58b758de2cb0c4ae1fbacc53d69b2d27f
RathodDeven <devenrathodrd@gmail.com>
2023-01-31 20:50:09 +0530

    ImageWithpulsing loadin in zom

--- components/Common/UI/ImageWithFullScreenZoom.js
+++ components/Common/UI/ImageWithFullScreenZoom.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Zoom from 'react-medium-image-zoom'
 import 'react-medium-image-zoom/dist/styles.css'
+import ImageWithPulsingLoader from './ImageWithPulsingLoader'
 
 const ImageWithFullScreenZoom = ({ ...props }) => {
   return (
     <Zoom classDialog="custom-zoom">
-      <img {...props} className={`${props.className}`} />
+      <ImageWithPulsingLoader {...props} className={`${props.className}`} />
     </Zoom>
   )
 }
