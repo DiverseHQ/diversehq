23cdc2cdfbbd03edd27127cc772857a352ac798b
alirmemon37 <alirmemona@gmail.com>
2023-02-11 16:43:13 +0530

    scroll to top functionality in bottomNav icons& UI

--- components/Home/MobileBottomNav.js
+++ components/Home/MobileBottomNav.js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import React, { useState, useEffect } from 'react'
 import useNotificationsCount from '../Notification/useNotificationsCount'
+
 const MobileBottomNav = () => {
   const {
     notificationsCount,
@@ -14,9 +15,15 @@ const MobileBottomNav = () => {
   const routeToHome = () => {
     router.push('/')
   }
+
   const routeToExplore = () => {
     router.push('/explore')
   }
+
+  const routeToSearch = () => {
+    router.push('/search')
+  }
+
   const routeToNotifications = async () => {
     await updateLensNotificationCount()
     router.push('/notification')
@@ -36,51 +43,90 @@ const MobileBottomNav = () => {
     }
   }, [pathname])
 
-  const routeToSearch = () => {
-    router.push('/search')
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
   }
 
   return (
     <div className="fixed bottom-0 w-full py-2 flex flex-row justify-evenly items-center bg-p-bg shadow-top min-h-[56px]">
-      <img
-        src={`${
-          active === 'home' ? '/mobileNavHomeFilled.svg' : '/mobileNavHome.svg'
-        }`}
-        alt="Home"
-        onClick={routeToHome}
-        className="cursor-pointer"
-      />
-      <img
-        src={`${
-          active === 'explore'
-            ? '/mobileNavExploreFilled.svg'
-            : '/mobileNavExplore.svg'
-        }`}
-        alt="Explore"
-        onClick={routeToExplore}
-        className="cursor-pointer"
-      />
-      <img
-        src={`${
-          active === 'search'
-            ? '/mobileNavSearchActive.svg'
-            : '/mobileNavSearch.svg'
-        }`}
-        alt="Search"
-        onClick={routeToSearch}
-        className="cursor-pointer"
-      />
-      <div className="relative">
+      <div
+        className="p-1.5 hover:bg-[#6668FF] rounded-full hover:bg-opacity-20 cursor-pointer"
+        onClick={() => {
+          if (router.pathname !== '/') {
+            routeToHome()
+            return
+          }
+          scrollToTop()
+        }}
+      >
         <img
           src={`${
-            active === 'notification'
-              ? '/mobileNavNotificationFilled.svg'
-              : '/mobileNavNotification.svg'
+            active === 'home'
+              ? '/mobileNavHomeFilled.svg'
+              : '/mobileNavHome.svg'
           }`}
-          alt="Notification"
-          onClick={routeToNotifications}
-          className="cursor-pointer"
+          alt="Home"
+          className="w-5.5 h-5.5"
         />
+      </div>
+      <div
+        className="p-1.5 hover:bg-[#6668FF] rounded-full hover:bg-opacity-20 cursor-pointer"
+        onClick={() => {
+          if (!router.pathname.startsWith('/explore')) {
+            routeToExplore()
+            return
+          }
+          scrollToTop()
+        }}
+      >
+        <img
+          src={`${
+            active === 'explore'
+              ? '/mobileNavExploreFilled.svg'
+              : '/mobileNavExplore.svg'
+          }`}
+          alt="Explore"
+          className="w-5.5 h-5.5"
+        />
+      </div>
+      <div
+        className="p-1.5 hover:bg-[#6668FF] rounded-full hover:bg-opacity-20 cursor-pointer"
+        onClick={routeToSearch}
+      >
+        <img
+          src={`${
+            active === 'search'
+              ? '/mobileNavSearchActive.svg'
+              : '/mobileNavSearch.svg'
+          }`}
+          alt="Search"
+          className="w-5.5 h-5.5"
+        />
+      </div>
+      <div
+        className="relative"
+        onClick={() => {
+          if (!router.pathname.startsWith('/notification')) {
+            routeToNotifications()
+            return
+          }
+          scrollToTop()
+        }}
+      >
+        <div className="p-1.5 hover:bg-[#6668FF] rounded-full hover:bg-opacity-20 cursor-pointer">
+          <img
+            src={`${
+              active === 'notification'
+                ? '/mobileNavNotificationFilled.svg'
+                : '/mobileNavNotification.svg'
+            }`}
+            alt="Notification"
+            className="w-[23px] h-[23px]"
+          />
+        </div>
         {notificationsCount + lensNotificationsCount > 0 && (
           <div className="absolute -top-1 left-3 leading-[4px] p-1 text-[8px] text-p-btn-text bg-red-500 font-bold rounded-full border-[2.5px] border-p-bg">
             <span>{notificationsCount + lensNotificationsCount}</span>
