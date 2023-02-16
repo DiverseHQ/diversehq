2634861b118ffc8c732b2fd61e62e9c412b6eac0
RathodDeven <devenrathodrd@gmail.com>
2023-02-16 21:08:56 +0530

    loaders

--- components/Post/LensPostsExplorePublicationsColumn.js
+++ components/Post/LensPostsExplorePublicationsColumn.js
@@ -186,13 +186,17 @@ const LensPostsExplorePublicationsColumn = () => {
             <MobileLoader />
           ) : (
             <>
-              <div className="w-full sm:rounded-2xl h-[300px] sm:h-[450px] bg-gray-100 dark:bg-s-bg animate-pulse my-3 sm:my-6">
-                <div className="w-full flex flex-row items-center space-x-4 p-4">
-                  <div className="w-8 h-8 sm:w-12 sm:h-12 bg-gray-300 dark:bg-p-bg rounded-full animate-pulse" />
-                  <div className="h-2 sm:h-4 w-[100px] sm:w-[200px] rounded-full bg-gray-300 dark:bg-p-bg" />
-                  <div className="h-2 sm:h-4 w-[50px] rounded-full bg-gray-300 dark:bg-p-bg" />
+              <div className="w-full sm:rounded-2xl h-[300px] sm:h-[450px] bg-s-bg dark:bg-s-bg my-3 sm:my-6">
+                <div className="w-full flex flex-row items-center space-x-4 p-2 px-4 animate-pulse">
+                  <div className="w-8 h-8 sm:w-12 sm:h-12 bg-gray-300 dark:bg-p-bg rounded-full " />
+                  <div className="h-4 w-[100px] sm:w-[200px] rounded-full bg-gray-300 dark:bg-p-bg" />
+                  <div className="h-4 w-[50px] rounded-full bg-gray-300 dark:bg-p-bg" />
                 </div>
-                <div className="w-full flex flex-row items-center space-x-4 sm:p-4 pr-4">
+                <div className="w-full flex flex-row items-center space-x-4 sm:p-4 sm:py-2 py-1 pr-4 my-1 animate-pulse">
+                  <div className="w-6 sm:w-[50px] h-4" />
+                  <div className="w-full rounded-xl bg-gray-300 dark:bg-p-bg h-[20px] sm:h-[20px]" />
+                </div>
+                <div className="w-full flex flex-row items-center space-x-4 sm:p-4 sm:pb-2 pr-4 animate-pulse">
                   <div className="w-6 sm:w-[50px] h-4" />
                   <div className="w-full rounded-2xl bg-gray-300 dark:bg-p-bg h-[200px] sm:h-[300px]" />
                 </div>
@@ -210,13 +214,17 @@ const LensPostsExplorePublicationsColumn = () => {
           (exploreQueryRequestParams.posts.length === 0 &&
             exploreQueryRequestParams.hasMore)) && (
           <>
-            <div className="w-full sm:rounded-2xl h-[300px] sm:h-[450px] bg-gray-100 dark:bg-s-bg animate-pulse my-3 sm:my-6">
-              <div className="w-full flex flex-row items-center space-x-4 p-4">
-                <div className="w-8 h-8 sm:w-12 sm:h-12 bg-gray-300 dark:bg-p-bg rounded-full animate-pulse" />
-                <div className="h-2 sm:h-4 w-[100px] sm:w-[200px] rounded-full bg-gray-300 dark:bg-p-bg" />
-                <div className="h-2 sm:h-4 w-[50px] rounded-full bg-gray-300 dark:bg-p-bg" />
+            <div className="w-full sm:rounded-2xl h-[300px] sm:h-[450px] bg-s-bg dark:bg-s-bg my-3 sm:my-6">
+              <div className="w-full flex flex-row items-center space-x-4 p-2 px-4 animate-pulse">
+                <div className="w-8 h-8 sm:w-12 sm:h-12 bg-gray-300 dark:bg-p-bg rounded-full " />
+                <div className="h-4 w-[100px] sm:w-[200px] rounded-full bg-gray-300 dark:bg-p-bg" />
+                <div className="h-4 w-[50px] rounded-full bg-gray-300 dark:bg-p-bg" />
               </div>
-              <div className="w-full flex flex-row items-center space-x-4 sm:p-4 pr-4">
+              <div className="w-full flex flex-row items-center space-x-4 sm:p-4 sm:py-2 py-1 pr-4 my-1 animate-pulse">
+                <div className="w-6 sm:w-[50px] h-4" />
+                <div className="w-full rounded-xl bg-gray-300 dark:bg-p-bg h-[20px] sm:h-[20px]" />
+              </div>
+              <div className="w-full flex flex-row items-center space-x-4 sm:p-4 sm:pb-2 pr-4 animate-pulse">
                 <div className="w-6 sm:w-[50px] h-4" />
                 <div className="w-full rounded-2xl bg-gray-300 dark:bg-p-bg h-[200px] sm:h-[300px]" />
               </div>
