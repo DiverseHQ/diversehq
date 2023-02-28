07155998e79af31fa4e874b073a659b04d504774
alirmemon37 <alirmemona@gmail.com>
2023-02-28 15:15:44 +0530

    Audio player UI update

--- components/Post/CoverImage.tsx
+++ components/Post/CoverImage.tsx
@@ -3,11 +3,11 @@ import ImageWithFullScreenZoom from '../Common/UI/ImageWithFullScreenZoom'
 
 const CoverImage = ({ coverImage }) => {
   return (
-    <div className="h-24 w-24 md:h-40 md:w-60 md:rounded-none">
+    <div className="h-32 w-28 md:h-40 md:w-60 md:rounded-none">
       <ImageWithFullScreenZoom
         src={coverImage}
         alt="image"
-        className="image-unselectable object-cover h-24 w-24 md:h-40 md:w-60"
+        className="image-unselectable object-cover h-32 w-28 md:h-40 md:w-60"
       />
     </div>
   )
