013eed643ff96aebf9527a8e8efef5804c7c901a
alirmemon37 <alirmemona@gmail.com>
2023-03-06 16:47:13 +0530

    community name validation added

--- components/Common/UI/FormTextInput.js
+++ components/Common/UI/FormTextInput.js
@@ -6,14 +6,18 @@ const FormTextInput = ({
   value,
   maxLength = null,
   disabled = false,
+  // onBlur,
+  errorMsg = null,
   ...props
 }) => {
   return (
     <label className={`${disabled ? 'cursor-not-allowed' : ''}`}>
       <div
-        className={`border rounded-xl border-s-border mx-4 py-2 px-4 my-4 text-p-text bg-s-bg ${
+        className={`relative border rounded-xl mx-4 py-2 px-4 my-4 text-p-text bg-s-bg ${
           disabled ? 'cursor-not-allowed' : ''
-        }`}
+        } ${
+          errorMsg ? 'border-red-500' : 'border-s-border'
+        } `}
       >
         <div className="pb-2">{label}</div>
         <div className="flex flex-row items-center justify-between">
@@ -25,6 +29,7 @@ const FormTextInput = ({
             className={`w-full ${
               disabled ? 'text-s-text cursor-not-allowed' : 'text-p-text'
             } bg-s-bg outline-none ${className}`}
+            // onBlur={onBlur}
             {...props}
           />
           {maxLength && (
@@ -32,6 +37,13 @@ const FormTextInput = ({
               {value ? value?.length : 0}/{maxLength}
             </div>
           )}
+          {
+            errorMsg && (
+              <div className="text-red-500 text-xs absolute top-[8px] right-[8px]">
+                {errorMsg}
+              </div>
+            )
+          }
         </div>
       </div>
     </label>
