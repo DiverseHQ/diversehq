42216f5962b2c80d0ebe8ea3520ade4e85e2b61d
RathodDeven <devenrathodrd@gmail.com>
2023-03-29 10:53:33 +0530

    api endpoint updated

--- api/ApiEndpoint.ts
+++ api/ApiEndpoint.ts
@@ -7,7 +7,10 @@ switch (mode) {
     apiEndpoint = 'http://localhost:8000/apiv1'
     break
   case 'production':
-    apiEndpoint = 'https://diverse-hq-api-diversehq.vercel.app/apiv1'
+    apiEndpoint = 'https://diverse-hq-api-alpha.vercel.app/apiv1'
+    break
+  case 'testnet':
+    apiEndpoint = 'https://diverse-hq-api-testnet.vercel.app/apiv1'
     break
   default:
     apiEndpoint = 'https://diverse-hq-api-diversehq.vercel.app/apiv1'
