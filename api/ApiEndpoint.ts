fda8d41ffe6c6957ce5120f73b40452cb786cac5
RathodDeven <devenrathodrd@gmail.com>
2023-04-01 12:33:01 +0530

    NEXT_PUBLIC_NODE_API_MODE

--- api/ApiEndpoint.ts
+++ api/ApiEndpoint.ts
@@ -1,5 +1,5 @@
 //eslint-disable-next-line
-const mode = process.env.NEXT_PUBLIC_NODE_MODE
+const mode = process.env.NEXT_PUBLIC_NODE_API_MODE
 // const mode = 'production'
 let apiEndpoint: string = ''
 switch (mode) {
