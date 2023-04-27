faf134baa2ddf7123ff071e7d1fadedd5799181c
RathodDeven <devenrathodrd@gmail.com>
2023-04-27 10:56:56 +0530

    encrypting token before sending

--- api/apiHelper.js
+++ api/apiHelper.js
@@ -1,13 +1,15 @@
 import { getAccessToken } from '../auth-fetcher'
+import { encrypt } from '../utils/utils'
 // import { getLocalToken } from '../utils/token'
 
 export const getHeaders = async () => {
   // let token = getLocalToken()
   let token = await getAccessToken()
+  const encryptedToken = encrypt(token)
   if (token) {
     return {
       'Content-Type': 'application/json',
-      Authorization: token
+      Authorization: encryptedToken
     }
   } else {
     return {
