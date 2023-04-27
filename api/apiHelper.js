051f50d8f5f50e16a01406fc64acea584201b128
RathodDeven <devenrathodrd@gmail.com>
2023-04-27 11:05:19 +0530

    Revert "encrypting token before sending"
    This reverts commit faf134baa2ddf7123ff071e7d1fadedd5799181c.

--- api/apiHelper.js
+++ api/apiHelper.js
@@ -1,15 +1,13 @@
 import { getAccessToken } from '../auth-fetcher'
-import { encrypt } from '../utils/utils'
 // import { getLocalToken } from '../utils/token'
 
 export const getHeaders = async () => {
   // let token = getLocalToken()
   let token = await getAccessToken()
-  const encryptedToken = encrypt(token)
   if (token) {
     return {
       'Content-Type': 'application/json',
-      Authorization: encryptedToken
+      Authorization: token
     }
   } else {
     return {
