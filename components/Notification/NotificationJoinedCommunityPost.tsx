2dfb9931e4586ebe3049ce313336a92d45bc733c
Rathod Deven <devenrathodrd@gmail.com>
2023-06-10 10:54:53 +0530

    loading in new posts notification

--- components/Notification/NotificationJoinedCommunityPost.tsx
+++ components/Notification/NotificationJoinedCommunityPost.tsx
@@ -12,7 +12,6 @@ const NotificationJoinedCommunityPost = ({
 }: {
   notification: NotificationSchema
 }) => {
-  console.log('notification', notification)
   const [post, setPost] = useState(null)
   const { data } = useLensUserContext()
 
@@ -33,42 +32,62 @@ const NotificationJoinedCommunityPost = ({
   useEffect(() => {
     fetchAndSetPost()
   }, [notification?.refId])
-  if (!post) return null
   return (
     <CommonNotificationCardLayoutUI
       MainRow={() => (
-        <div className="flex flex-row flex-wrap space-x-1">
+        <div className="flex flex-row items-center flex-wrap space-x-1">
           <span>New Post from</span>
           <span
             className="hover:underline font-bold"
             onClick={(e) => e.stopPropagation()}
           >
-            <Link href={`/u/${formatHandle(post?.profile?.handle)}`}>
-              <span>{`u/${formatHandle(post?.profile?.handle)}`}</span>
+            <Link
+              href={`/u/${formatHandle(
+                notification?.senderLensProfile?.handle
+              )}`}
+            >
+              <span>{`u/${formatHandle(
+                notification?.senderLensProfile?.handle
+              )}`}</span>
             </Link>
           </span>
           <span>in</span>
-          <span
-            className="hover:underline font-bold"
-            onClick={(e) => e.stopPropagation()}
-          >
-            {post?.isLensCommunityPost ? (
-              <Link href={`/l/${formatHandle(post?.communityInfo?.handle)}`}>
-                <span>{`l/${formatHandle(post?.communityInfo?.handle)}`}</span>
-              </Link>
-            ) : (
-              <Link href={`/c/${post?.communityInfo?.name}`}>
-                <span>{`c/${post?.communityInfo?.name}`}</span>
-              </Link>
-            )}
-          </span>
+
+          {post?.communityInfo ? (
+            <span
+              className="hover:underline font-bold"
+              onClick={(e) => e.stopPropagation()}
+            >
+              {post?.isLensCommunityPost ? (
+                <Link href={`/l/${formatHandle(post?.communityInfo?.handle)}`}>
+                  <span>{`l/${formatHandle(
+                    post?.communityInfo?.handle
+                  )}`}</span>
+                </Link>
+              ) : (
+                <Link href={`/c/${post?.communityInfo?.name}`}>
+                  <span>{`c/${post?.communityInfo?.name}`}</span>
+                </Link>
+              )}
+            </span>
+          ) : (
+            <span className="h-3 w-10 bg-s-text rounded-xl animate-pulse" />
+          )}
         </div>
       )}
       createdAt={notification?.createdAt}
-      Body={() => <div>{post?.metadata?.name}</div>}
+      Body={() => (
+        <>
+          {post?.metadata?.name ? (
+            <div>{post?.metadata?.name}</div>
+          ) : (
+            <div className="h-3 w-30 bg-s-text rounded-xl animate-pulse" />
+          )}
+        </>
+      )}
       Icon={() => <BsStars />}
       isRead={notification?.isRead}
-      cardLink={`/p/${post?.id}`}
+      cardLink={post?.id ? `/p/${post?.id}` : null}
     />
   )
 }
